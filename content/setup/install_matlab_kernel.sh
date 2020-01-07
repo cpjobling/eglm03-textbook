00509b359915adc426dd1f360207e878dce53a4f
Chris Jobling <cpjobling@gmail.com>
2020-01-07 09:14:15 +0000

    added technical user guide

--- content/setup/install_matlab_kernel.sh
+++ content/setup/install_matlab_kernel.sh
@@ -6,14 +6,16 @@ EGLM03=$HOME/dev/eglm03-textbook
 # Set up for jupyter with rise and jupyter-book
 cd $EGLM03
 conda update conda
-conda create -n eglm03-textbook python=3.7 anaconda
-conda activate eglm03-textbook
+conda create -n eglm03 python=3.7 anaconda
+conda activate eglm03
 conda install -c conda-forge rise
 pip install jupyter-book
 
 # Set up Python-MATLAB bridge and matlab_kernel
-python $MATLAB_HOME/extern/engines/python/setup.py install
+cd $MATLAB_HOME/extern/engines/python
+python setup.py install
 pip install matlab_kernel
+cd $EGLM03
 
 # Save environment and exit eg-247-textbook
 conda env export > environment.yml
